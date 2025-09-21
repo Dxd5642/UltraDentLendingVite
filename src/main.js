70f4e45d4fae22cbc2c01a3426120296c53ac98b
dxd <dxd56422@yandex.ru>
2025-09-21 22:13:50 +0500

    First block

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const header_mobile = document.querySelector('.head2');
 const btn_open_navbar = document.querySelector(".header__menu-mobile");
 const menu_icon = document.querySelector(".header__menu-mobile-img");
 const span_btn = document.querySelectorAll(".header_span")
+const body = document.querySelector("body")
 
 document.addEventListener('DOMContentLoaded', function() {
     btn_open_navbar.addEventListener('click', toggleNavbar);
@@ -14,6 +15,7 @@ function toggleNavbar() {
         header_mobile.classList.toggle('active');
         span_btn.forEach(i => {i.classList.toggle("active")})
         btn_open_navbar.classList.toggle('active')
+        body.classList.toggle('open-navbar')
     }
 }
 
